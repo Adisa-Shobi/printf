5a37d4fe6fd7c5e65095c6d1a05a7c4bce891020
kleanton1 <ifeanyiemmanuel271@gmail.com>
2022-05-22 18:30:32 +0000

    reverse string case specifier

---
+++ print_rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ * _print_rev_string - Prints reversed string
+ *
+ *@args: A va_list containing string
+ * Return: lenght of the string printed
+ */
+int _print_rev_string(va_list args)
+{
+	char *str;
+	int i, count = 0;
+
+	str = va_arg(args, char*);
+	if (str == NULL)
+		str = ")llun(";
+	i = _strlen(str);
+	for (i -= 1 ; i >= 0 ; i--)
+	{
+		_putchar(str[i]);
+		count++;
+	}
+	return (count);
+}
